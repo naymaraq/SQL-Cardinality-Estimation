520ab6cc3ee25ba139977c6ba9f23e745f34408e
Davit <davitkar98@gmail.com>
2020-07-28 15:01:52 +0400

    upload eval.py

---
+++ benchmark/evaluation/eval.py
@@ -0,0 +1,21 @@
+import argparse
+from benchmark.scripts.score import Score
+from benchmark.models.random.random import Random
+from benchmark.models.treernn.treernn_inference import TreeRNNInference
+from benchmark.models.histogram.histogram import HistogramEstimator
+from benchmark.models.hybrid.ensemble import Ensemble
+
+#random_model = Random()
+
+state = {"input_dim":6+7+263, "mem_dim":32,"hidden_dim":80}
+treernn_model = TreeRNNInference('models/treernn_best.pt', state=state)
+hist_est = HistogramEstimator('data/stats.json')
+ensemble = Ensemble([treernn_model, hist_est])
+
+if __name__ == "__main__":
+
+    sc = Score('data')
+    print(sc(treernn_model))
+    print(sc(hist_est))
+    print(sc(ensemble))
+
